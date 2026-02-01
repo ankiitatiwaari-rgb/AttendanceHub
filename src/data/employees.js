6cf0177f9d7729bbbd8e64c2db5fdb15d2398fef
TiwariAnku <at3858835@gmail.com>
2026-02-01 23:18:39 +0530

    Dummy entry

--- src/data/employees.js
+++ src/data/employees.js
@@ -10,7 +10,44 @@ export const employees = [
     id: "EMP-2026-084",
     username: "Shreeya",
     password: "123",
-    name: "Shreeya Tiwari",
+    name: "Shreeya",
     role: "Software Engineer"
-  }
+  },
+   {
+    id: "EMP-2026-084",
+    username: "Shani",
+    password: "123",
+    name: "Shreeya",
+    role: "Software Engineer"
+  },
+   {
+    id: "EMP-2026-084",
+    username: "Amit",
+    password: "123",
+    name: "Shreeya",
+    role: "Software Engineer"
+  },
+   {
+    id: "EMP-2026-084",
+    username: "Rahul",
+    password: "123",
+    name: "Shreeya",
+    role: "Software Engineer"
+  },
+   {
+    id: "EMP-2026-084",
+    username: "Sakhi",
+    password: "123",
+    name: "Shreeya",
+    role: "Software Engineer"
+  },
+   {
+    id: "EMP-2026-084",
+    username: "Madhura",
+    password: "123",
+    name: "Shreeya",
+    role: "Software Engineer"
+  },
+  
+
 ];
